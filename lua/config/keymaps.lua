8f735ab435fe02f25fe53b4029326fb2cd4abbcd
Thibo Verbeerst <thibo@verbeerst.com>
2024-07-08 11:16:49 +0200

    moved vim.g settings to keymaps and options to keep it more organised

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,4 +1,10 @@
 
+-- Set <space> as the leader key
+-- -- See `:help mapleader`
+-- --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
 -- Clear search highlight on pressing <Esc> in normal mode
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
