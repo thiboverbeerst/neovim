530fa8117843f7955921d037fb36a4272941c3ef
Thibo Verbeerst <thibo@verbeerst.com>
2024-07-08 02:25:33 +0200

    added initial neovim conf based kickstart.nvim and Allaman/nvim

---
+++ init.lua
@@ -0,0 +1,8 @@
+-- References to ./lua/
+
+-- User configurations
+require("config")
+
+-- Load Neovim features using plugin manager of choice
+-- Current plugin manager: lazy.nvim
+require("core.lazy") 
