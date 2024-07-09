33240773e194b057ae1b9f91a49ce3e7eb9488ea
Thibo Verbeerst <thibo@verbeerst.com>
2024-07-10 00:59:27 +0200

    added trouble conf

---
+++ lua/core/plugins/trouble.lua
@@ -0,0 +1,27 @@
+
+-- A pretty list for showing diagnostics, references, telescope results, quickfix and location lists to help you solve all the trouble your code is causing.
+-- https://github.com/folke/trouble.nvim
+
+return {
+  {
+    "folke/trouble.nvim",
+    config = function()
+      require("trouble").setup({
+        icons = false,
+      })
+
+      vim.keymap.set("n", "<leader>tt", function()
+        require("trouble").toggle()
+      end)
+
+      vim.keymap.set("n", "[t", function()
+        require("trouble").next({skip_groups = true, jump = true});
+      end)
+
+      vim.keymap.set("n", "]t", function()
+        require("trouble").previous({skip_groups = true, jump = true});
+      end)
+
+    end
+  },
+}
