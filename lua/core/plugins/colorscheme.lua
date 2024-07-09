47a67bff023d9dda8f1bea1d52e697fac147ec10
Thibo Verbeerst <thibo@verbeerst.com>
2024-07-10 00:20:31 +0200

    removed hi command

--- lua/core/plugins/colorscheme.lua
+++ lua/core/plugins/colorscheme.lua
@@ -11,9 +11,6 @@ return {
     -- Like many other themes, this one has different styles, and you could load
     -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
     vim.cmd.colorscheme 'catppuccin'
-
-    -- You can configure highlights by doing something like:
-    vim.cmd.hi 'Comment gui=none'
   end,
   config = function()
     require("catppuccin").setup({
