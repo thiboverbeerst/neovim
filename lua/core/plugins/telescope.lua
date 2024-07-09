b41eeb67d2140e3e7652d358b6a32d47fc3722b9
Thibo Verbeerst <thibo@verbeerst.com>
2024-07-09 02:20:24 +0200

    fixing keymaps for telescope

--- lua/core/plugins/telescope.lua
+++ lua/core/plugins/telescope.lua
@@ -1,7 +1,24 @@
+local funcs = require("utils.functions")
+
 
 -- [ Why configure plugin in a core.plugins file? ]
 -- When settings are closer to the plugin it's less prone to errors
 -- For eg, settings aren't loaded when plugin is disabled
+
+
+local function neovim_conf_files()
+  return "lua require('telescope.builtin').find_files({ cwd = require('lazy.core.config').options.root })"
+end
+
+
+local function project_files()
+  local path = vim.loop.cwd() .. "/.git"
+  if funcs.path_exists(path) then
+    return "lua require('telescope.builtin').git_files({ show_untracked = true })"
+  else
+    return "Telescope find_files"
+  end
+end
 
 
 -- [1] Telescope highly extendable fuzzy finder over lists.
@@ -17,11 +34,9 @@ return {
     dependencies = { 'nvim-lua/plenary.nvim' },
     keys = {
       -- add a keymap to browse plugin files
-      {
-        "<leader>fp",
-        function() require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root }) end,
-        desc = "Find Plugin File",
-      },
+      { "<leader>fp", "<cmd>" .. neovim_conf_files() .. "<cr>", desc = "Find Neovim Config File" },
+      { "<leader>ff", "<cmd>" .. project_files() .. "<cr>", desc = "Find File" },
+      { "<leader>fF", "<cmd>Telescope find_files<cr>", desc = "Find File (ignore git)" },
     },
     opts = {
       defaults = {
---
+++ lua/utils/functions.lua
@@ -0,0 +1,35 @@
+local funcs = {}
+
+---notify
+---@param message string
+---@param level integer
+---@param title string
+funcs.notify = function(message, level, title)
+  local notify_options = {
+    title = title,
+    timeout = 2000,
+  }
+  vim.api.nvim_notify(message, level, notify_options)
+end
+
+-- This function safely navigates through a nested configuration table to retrieve a value. 
+-- It takes a configuration table (config) and a variable number of additional arguments (...) representing
+-- the path to the desired value within the table. It returns nil if any part of the path does not exist.
+funcs.safe_nested_config = function(config, ...)
+  local elements = { ... }
+  local node = config
+  for i = 1, #elements do
+    node = node[elements[i]]
+    if node == nil then
+      return nil
+    end
+  end
+  return node
+end
+
+--- Check if path exists
+funcs.path_exists = function(path)
+  return vim.loop.fs_stat(path)
+end
+
+return funcs
