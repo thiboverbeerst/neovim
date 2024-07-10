11c3abb249e3586d7111feb13cdf804e38e63376
Thibo Verbeerst <thibo@verbeerst.com>
2024-07-10 22:25:18 +0200

    added autopairs plugin

---
+++ lua/core/plugins/autopairs.lua
@@ -0,0 +1,10 @@
+
+-- A super powerful autopair plugin for Neovim that supports multiple characters.
+-- https://github.com/windwp/nvim-autopairs
+return {
+  'windwp/nvim-autopairs',
+  event = "InsertEnter",
+  config = true,
+  -- use opts = {} for passing setup options
+  -- this is equalent to setup({}) function
+}
