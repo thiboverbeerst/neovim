f551c6808642f715d7e03d4d3d1ddc8cd93725d2
Thibo Verbeerst <thibo@verbeerst.com>
2024-07-11 18:23:14 +0200

    added indent blankline plugin and installed it

---
+++ lua/core/plugins/indent-blankline.lua
@@ -0,0 +1,5 @@
+
+-- This plugin adds indentation guides to Neovim. It uses Neovim's virtual text feature and no conceal
+-- https://github.com/lukas-reineke/indent-blankline.nvim
+
+return { "lukas-reineke/indent-blankline.nvim", main = "ibl", opts = {} }
